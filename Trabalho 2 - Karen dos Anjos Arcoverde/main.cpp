c8fbc368586179cec9df3a5ef096d3d3ccbde360
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2021-05-02 23:43:25 -0300

    Add files via upload

--- Trabalho 2 - Karen dos Anjos Arcoverde/main.cpp
+++ Trabalho 2 - Karen dos Anjos Arcoverde/main.cpp
@@ -12,3 +12,37 @@ using namespace std;
    Programa da construção de um grafo
    Autora: Karen dos Anjos Arcoverde */
 
+int main(){
+
+   Vertice vertice;
+   //Lê o arquivo e guarda cada palavra lida em um vetor de strings
+   fstream file;
+   string nomeArquivo;
+
+   nomeArquivo = "texto.txt";
+   vector <string> conjuntoDePalavras;
+
+   file.open (nomeArquivo, fstream::in);
+
+   if (!file.is_open()){
+      cout << "Arquivo nao existe." << endl;
+      return; 
+   }
+
+   while (file.good()){
+      string linha;
+
+      getline (file, linha, ' ');
+      if ((linha[linha.length()-1] == '.') || (linha[linha.length()-1] == ','))
+		linha = linha.substr(0,linha.length()-1);
+
+      vertice.setPalavra (linha);
+	}
+   file.close();
+
+
+
+
+
+   return 0;
+}
