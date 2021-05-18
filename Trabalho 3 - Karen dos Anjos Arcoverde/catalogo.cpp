72ba17aa39ffb8b315c9f23f4ea82cf1615585ed
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2021-05-17 23:31:09 -0300

    Add files via upload

---
+++ Trabalho 3 - Karen dos Anjos Arcoverde/catalogo.cpp
@@ -0,0 +1,25 @@
+/*****************************************************************************/
+/***************************** Arquivo catalogo.cpp **************************/
+
+#include "catalogo.h"
+
+ostream &operator<<(ostream &saida, const Catalogo &catalogo){
+    for (int indice_filmes = 0; indice_filmes < catalogo.conjuntoDeFilmes.size(); indice_filmes++)
+        saida << catalogo.conjuntoDeFilmes.at(indice_filmes);
+    
+    return saida;
+};
+
+bool operator>(const filme &primfilme, const filme &segfilme){
+    if (primfilme.nomeDoFilme > segfilme.nomeDoFilme) 
+        return true;
+
+    return false;
+};
+
+bool operator==(const filme &primfilme, const filme &segfilme){
+    if (primfilme.nomeDoFilme == segfilme.nomeDoFilme) 
+        return true;
+
+    return false;
+};
---
+++ Trabalho 3 - Karen dos Anjos Arcoverde/main.cpp
@@ -0,0 +1,23 @@
+/***************************************************************************/
+/*************************** Programa Principal ****************************/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include "catalogo.h"
+
+using namespace std;
+
+/* Programa do Trabalho 3: 
+   Programa de um catalogo de filmes
+   Autora: Karen dos Anjos Arcoverde */
+
+int main(){
+
+
+
+
+
+
+    return 0;
+}
