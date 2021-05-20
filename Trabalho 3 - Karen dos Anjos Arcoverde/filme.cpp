7ec847e4ac04761b8347dd9b252733c6eeed40cd
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2021-05-20 11:46:51 -0300

    Add files via upload

--- Trabalho 3 - Karen dos Anjos Arcoverde/filme.cpp
+++ Trabalho 3 - Karen dos Anjos Arcoverde/filme.cpp
@@ -16,9 +16,9 @@ istream &operator>>(istream &entrada, filme &item_filme){
 }
 
 ostream &operator<<(ostream &saida, const filme &item_filme){
-    saida << "\tFilme: " << item_filme.nomeDoFilme << endl;
-    saida << "\tProdutora: " << item_filme.nomeDaProdutora << endl;
-    saida << "\tNota: " << item_filme.notaDoFilme << endl;
+    saida << "Filme: " << item_filme.nomeDoFilme << endl;
+    saida << "Produtora: " << item_filme.nomeDaProdutora << endl;
+    saida << "Nota: " << item_filme.notaDoFilme << endl;
     saida << endl;
 
     return saida;
